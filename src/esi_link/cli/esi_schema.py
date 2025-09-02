08a33761ff303f6fdfec86ce7766b6a9570a98c8
Chad Lowe <pfmsoft.dev@gmail.com>
2025-09-02 10:43:28 -0700

    Add cli endpoint for schema

---
+++ src/esi_link/cli/esi_schema.py
@@ -0,0 +1,75 @@
+from pathlib import Path
+from typing import Annotated
+
+import typer
+
+from esi_link import CONFIG
+from esi_link.cli.helpers import filter_if_silent
+from esi_link.esi_schema.schema_store import SchemaStore
+
+app = typer.Typer(no_args_is_help=True)
+
+
+@app.command()
+def update(
+    ctx: typer.Context,
+    file_path: Annotated[
+        Path | None, typer.Option(help="Non-standard save path for the Schema Store.")
+    ] = None,
+):
+    """Update an existing ESI schema, or download a new one."""
+    msg = filter_if_silent(False)
+    try:
+        if file_path is None:
+            # work with app schema store
+            store: SchemaStore | None = ctx.obj.schema_store
+            if store is None:
+                msg("No existing schema store found, downloading new schema.")
+                store_path = CONFIG.schema_dir / "schema_store.json"
+                msg(f"Downloading schema to {store_path}")
+                store = SchemaStore.from_download(store_path=store_path)
+                msg("Download complete.")
+            else:
+                msg(f"Updating existing schema store at {store._store_path}.")  # type: ignore
+                store.update()
+                msg("Download complete.")
+        else:
+            # work with a schema store outside of the app
+            store_path = file_path
+            if store_path.is_file():
+                msg(f"Attempting to update existing schema store at {store_path}.")
+                store = SchemaStore(store_path=store_path)
+                msg(f"Downloading schema to {store_path}")
+                store.update()
+                msg("Download complete.")
+            else:
+                if store_path.is_dir():
+                    raise ValueError(
+                        f"Provided schema store path {store_path} is a directory."
+                    )
+                msg(f"Downloading schema to {store_path}")
+                store = SchemaStore.from_download(store_path=store_path)
+                msg("Download complete.")
+    except Exception as e:
+        typer.echo(f"Error updating schema: {e}")
+        raise typer.Exit(code=1) from e
+    msg("Schema updated successfully.")
+
+
+@app.command()
+def status(ctx: typer.Context):
+    """Show the current status of the ESI schema."""
+    msg = filter_if_silent(False)
+    msg("Current status of the ESI schema:")
+    if ctx.obj.schema_store:
+        store = ctx.obj.schema_store
+        msg(f"  Schema ID: {store._store_data.id_}")
+        msg(f"  Download Date: {store._store_data.download_date}")
+    else:
+        msg("No schema store found.")
+
+
+@app.command()
+def operations():
+    """Show available operations for the ESI schema."""
+    pass
--- src/esi_link/cli/main_typer.py
+++ src/esi_link/cli/main_typer.py
@@ -1,35 +1,95 @@
 """Command-line interface."""
 
+from dataclasses import dataclass
 from time import perf_counter_ns
 from typing import Annotated
 
 import typer
 
 from esi_link import CONFIG
+from esi_link.esi_schema.schema_store import SchemaStore
+from esi_link.helpers.indent_lines import indent_lines
+
+from .esi_schema import app as schema_app
+
+app = typer.Typer(no_args_is_help=True)
+app.add_typer(schema_app, name="schema", help="Manage ESI schema.")
 
 
+@dataclass
+class CliConfig:
+    start_time: int = perf_counter_ns()
+    debug: bool = False
+    verbosity: int = 1
+    schema_store: SchemaStore | None = None
+    silent: bool = False
+
+    def __repr__(self) -> str:
+        return (
+            f"CliConfig(start_time={self.start_time}, "
+            f"debug={self.debug}, "
+            f"verbosity={self.verbosity}, "
+            f"schema_store={self.schema_store}, "
+            f"silent={self.silent})"
+        )
+
+    def __str__(self) -> str:
+        return (
+            f" start_time={self.start_time}\n"
+            f" debug={self.debug}\n"
+            f" verbosity={self.verbosity}\n"
+            f" schema_store={self.schema_store}\n"
+            f" silent={self.silent}"
+        )
+
+
+@app.callback(invoke_without_command=True)
 def default_options(
     ctx: typer.Context,
     debug: Annotated[bool, typer.Option(help="Enable debug output.")] = False,
     verbosity: Annotated[int, typer.Option("-v", help="Verbosity.", count=True)] = 1,
+    silent: Annotated[
+        bool,
+        typer.Option(help="Enable silent mode. Only results and errors will be shown."),
+    ] = False,
 ):
     """Esi Link Command Line Interface.
 
     Insert pithy saying here
     """
-    ctx.ensure_object(dict)
-    ctx.obj["START_TIME"] = perf_counter_ns()
-    ctx.obj["DEBUG"] = debug
-    ctx.obj["VERBOSITY"] = verbosity
+    ctx.ensure_object(CliConfig)
+    ctx.obj.start_time = perf_counter_ns()
+    ctx.obj.debug = debug
+    ctx.obj.verbosity = verbosity
+    ctx.obj.silent = silent
+    ctx.obj.schema_store = load_schema()
 
-    if ctx.obj["VERBOSITY"] > 1:
+    if ctx.obj.schema_store:
+        schema_msg = "Schema loaded successfully."
+    else:
+        schema_msg = "No schema found. Try `esi-link schema update`."
+
+    welcome = f"""
+    Welcome to Esi Link! Your CLI interface to the Eve Online ESI api.
+    Application data located at {CONFIG.app_dir}
+    Schema status: {schema_msg}
+    """
+    if not ctx.obj.silent:
+        typer.echo(welcome)
+
+    if ctx.obj.verbosity > 1:
+        typer.echo("CLI configuration:")
+        typer.echo(f"{indent_lines(str(ctx.obj), indent=2)}")
         typer.echo("App configuration:")
-        typer.echo(f"{debug=}")
-        typer.echo(f"{verbosity=}")
-        typer.echo(f"{CONFIG=!r}")
+        typer.echo(f"{indent_lines(str(CONFIG), indent=2)}")
 
 
-app = typer.Typer(callback=default_options)
+def load_schema() -> SchemaStore | None:
+    """Load the ESI schema."""
+    store_path = CONFIG.schema_dir / "schema_store.json"
+    if not store_path.exists():
+        return None
+    return SchemaStore(store_path=store_path)
 
 
 if __name__ == "__main__":
