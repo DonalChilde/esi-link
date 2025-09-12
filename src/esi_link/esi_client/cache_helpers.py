d613a126c53ac30c63860c270d0df029acb5d4cc
Chad Lowe <pfmsoft.dev@gmail.com>
2025-09-12 12:13:51 -0700

    add is_cachable function, and refactor variable names

--- src/esi_link/esi_client/cache_helpers.py
+++ src/esi_link/esi_client/cache_helpers.py
@@ -1,22 +1,35 @@
 from uuid import UUID
 
 from esi_link.esi_client.models import EsiQuery, LinkCacheMetadata, QueryResponse
+from esi_link.esi_schema import operation_accessors as OA
 from esi_link.esi_schema.esi_api_protocol import EsiApiProtocol
 from esi_link.helpers import header_funcs as HF
 from esi_link.helpers.cache_id_from_url import cache_id_from_url
 
 
-def make_cache_key(query: EsiQuery, schema_api: EsiApiProtocol) -> UUID:
+def is_cachable(query: EsiQuery, esi_api: EsiApiProtocol) -> bool:
+    """Determine if the given query is cachable.
+
+    Args:
+        query (EsiQuery): The ESI query.
+        esi_api (EsiApiProtocol): The ESI API schema.
+    """
+    operation = esi_api.indexed_operation(query.operation_id)
+    cachable = all((operation.method.lower() == "get", not query.is_paged_subquery))
+    return cachable
+
+
+def make_cache_key(query: EsiQuery, esi_api: EsiApiProtocol) -> UUID:
     """Generate a unique cache key for the given query and schema.
 
     Args:
         query (EsiQuery): The ESI query.
-        schema (EveOpenApiProtocol): The ESI schema.
+        esi_api (EsiApiProtocol): The ESI API schema.
 
     Returns:
         UUID: The cache key for the query.
     """
-    url = schema_api.build_url(
+    url = esi_api.build_url(
         operation_id=query.operation_id,
         path_params=query.path_parameters,
         query_params=query.query_parameters,
@@ -29,18 +42,19 @@ def make_cache_key(query: EsiQuery, schema_api: EsiApiProtocol) -> UUID:
 def build_metadata(
     query: EsiQuery,
     response: QueryResponse,
-    schema_api: EsiApiProtocol,
+    esi_api: EsiApiProtocol,
 ) -> LinkCacheMetadata:
     """Build cache metadata from the query and response.
 
     Args:
         query (EsiQuery): The ESI query.
         response (QueryResponse): The ESI response.
+        esi_api (EsiApiProtocol): The ESI API schema.
 
     Returns:
         LinkCacheMetadata: The cache metadata.
     """
-    cache_key = make_cache_key(query, schema_api=schema_api)
+    cache_key = make_cache_key(query, esi_api=esi_api)
     expires = HF.expires(response.headers)
     etag = HF.etag(response.headers)
     last_modified_value = HF.last_modified(response.headers)
