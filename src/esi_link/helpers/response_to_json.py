519ed6d20192a7098b35980b55cb119faf772de0
Chad Lowe <pfmsoft.dev@gmail.com>
2025-09-12 08:28:28 -0700

    response_to_json handles paged and unpaged responses

--- src/esi_link/helpers/response_to_json.py
+++ src/esi_link/helpers/response_to_json.py
@@ -1,31 +1,43 @@
 import json
+import logging
 from typing import Any
 
 from esi_link.esi_client.models import QueryResponse
 
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
 
-def response_to_json(response: QueryResponse) -> dict[str, Any]:
-    """Convert a QueryResponse.text to a JSON-serializable dictionary.
+
+def response_to_json(response: QueryResponse) -> Any:
+    """Convert a QueryResponse.text to a JSON-serializable object.
 
     NOTE: This function assumes that the response text is a valid JSON string.
-    NOTE: This also assumes that the paged_text is a dict.
-    FIXME: This function currently does not handle non-JSON responses gracefully.
+    NOTE: This also assumes that the paged_text is a list.
     FIXME: Needs to handle json that might be the error report for a bad request.
     """
-
     try:
-        first_page_data = json.loads(response.text) if response.text else {}
-    except json.JSONDecodeError:
-        first_page_data: dict[str, Any] = {}
+        json_response = json.loads(response.text) if response.text else None
+    except json.JSONDecodeError as e:
+        logger.warning("Failed to decode JSON from response.text %r", response)
+        raise e from e
     if response.paged_text:
-        data_list: list[dict[str, Any]] = []
+        if not isinstance(json_response, list):
+            # FIXME raise EsiLink custom error here
+            msg = f"Expected response.text to be a list when paged_text is present, got {type(json_response)}"
+            logger.warning(msg)
+            raise TypeError(msg)
         for page in response.paged_text:
             try:
-                page_data = json.loads(page) if page else {}
-            except json.JSONDecodeError:
-                page_data: dict[str, Any] = {}
-            data_list.append(page_data)
-        first_page_data.update(
-            {key: value for d in data_list for key, value in d.items()}
-        )
-    return first_page_data
+                page_data = json.loads(page) if page else None
+            except json.JSONDecodeError as e:
+                logger.warning(
+                    "Failed to decode JSON from response.paged_text %r", response
+                )
+                raise e from e
+            if not isinstance(page_data, list):
+                # FIXME raise EsiLink custom error here
+                msg = f"Expected each page in response.paged_text to be a list, got {type(page_data)}"
+                logger.warning(msg)
+                raise TypeError(msg)
+            json_response.extend(page_data)
+    return json_response
