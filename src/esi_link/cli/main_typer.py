74c87b52f125df60017f150adb0ec80ab1ed2d64
Chad Lowe <pfmsoft.dev@gmail.com>
2025-09-14 15:43:00 -0700

    esi client required objects setup at cli start

--- src/esi_link/cli/main_typer.py
+++ src/esi_link/cli/main_typer.py
@@ -7,6 +7,13 @@ from typing import Annotated
 import typer
 
 from esi_link import CONFIG
+from esi_link.config import CacheType
+from esi_link.esi_client.esi_client import EsiClient
+from esi_link.esi_client.esi_file_cache import EsiFileCache
+from esi_link.esi_client.esi_memory_cache import EsiMemoryCache
+from esi_link.esi_client.link_cache_protocol import LinkCacheProtocol
+from esi_link.esi_schema.esi_api import EsiApi
+from esi_link.esi_schema.esi_api_protocol import EsiApiProtocol
 from esi_link.esi_schema.schema_store import SchemaStore
 from esi_link.helpers.indent_lines import indent_lines
 
@@ -24,6 +31,9 @@ class CliConfig:
     debug: bool = False
     verbosity: int = 1
     schema_store: SchemaStore | None = None
+    esi_api: EsiApiProtocol | None = None
+    cache: LinkCacheProtocol | None = None
+    client: EsiClient | None = None
     silent: bool = False
 
     def __repr__(self) -> str:
@@ -64,7 +74,7 @@ def default_options(
     ctx.obj.debug = debug
     ctx.obj.verbosity = verbosity
     ctx.obj.silent = silent
-    ctx.obj.schema_store = load_schema()
+    init_config(ctx)
 
     if ctx.obj.schema_store:
         schema_msg = "Schema loaded successfully."
@@ -86,12 +96,41 @@ def default_options(
         typer.echo(f"{indent_lines(str(CONFIG), indent=2)}")
 
 
-def load_schema() -> SchemaStore | None:
+def load_schema(ctx: typer.Context) -> SchemaStore:
     """Load the ESI schema."""
     store_path = CONFIG.schema_dir / "schema_store.json"
     if not store_path.exists():
-        return None
-    return SchemaStore(store_path=store_path)
+        store = SchemaStore.from_download(store_path=store_path)
+        typer.echo(
+            f"Schema downloaded and store created on {store.download_date}, saved to {store_path}"
+        )
+        return store
+    store = SchemaStore(store_path=store_path)
+    typer.echo(
+        f"Schema store with download date of {store.download_date} loaded from {store_path}"
+    )
+    return store
+
+
+def init_config(ctx: typer.Context) -> None:
+    """Initialize the CLI configuration with cache and client."""
+    ctx.obj.schema_store = load_schema(ctx)
+    ctx.obj.esi_api = EsiApi.from_schema_store(ctx.obj.schema_store)
+    # TODO option to clear cache entries older than X days
+    if CONFIG.cache_type == CacheType.FILE:
+        ctx.obj.cache = EsiFileCache(file_path=CONFIG.cache_file)
+    elif CONFIG.cache_type == CacheType.MEMORY:
+        ctx.obj.cache = EsiMemoryCache()
+    elif CONFIG.cache_type == CacheType.SQLITE:
+        raise NotImplementedError("SQLite cache is not implemented yet.")
+    else:
+        raise ValueError(f"Unknown cache type: {CONFIG.cache_type}")
+
+    ctx.obj.client = EsiClient(
+        esi_api=ctx.obj.esi_api,
+        cache=ctx.obj.cache,
+        max_concurrent_requests=50,
+    )
 
 
 if __name__ == "__main__":
