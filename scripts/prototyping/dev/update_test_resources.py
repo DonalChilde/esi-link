38239162f0141dc35639e9ccee7233fa8f8b0c66
Chad Lowe <pfmsoft.dev@gmail.com>
2025-09-17 07:53:49 -0700

    stub script for updating test rescources

---
+++ scripts/prototyping/dev/update_test_resources.py
@@ -0,0 +1,11 @@
+"""This script downloads the latest ESI API schema and updates local test resources."""
+
+SCHEMA_URL = "https://esi.evetech.net/meta/openapi.json"
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
