eaeefb55c21f3a68736d5f87f4c4d9add6c81999
Chad Lowe <pfmsoft.dev@gmail.com>
2025-09-17 18:29:51 -0700

    feat: add operation parameters table and response panel to test script

--- scripts/prototyping/test_operation_detail_table_output.py
+++ scripts/prototyping/test_operation_detail_table_output.py
@@ -5,9 +5,12 @@ from pathlib import Path
 
 from rich.console import Console
 
+from esi_link.esi_schema import operation_accessors as OA
 from esi_link.esi_schema.esi_api import EsiApi
 from esi_link.esi_schema.operation_formatters.format_operation_details import (
     operation_detail_table,
+    operation_parameters_table,
+    operation_response_200_panel,
 )
 from esi_link.esi_schema.schema_store import SchemaStore
 
@@ -25,8 +28,12 @@ def main():
         "GetCharactersCharacterIdWalletTransactions"
     )
     detail_table = operation_detail_table(indexed_operation)
+    request_param_table = operation_parameters_table(indexed_operation)
+    response_panel = operation_response_200_panel(indexed_operation)
     console = Console()
     console.print(detail_table)
+    console.print(request_param_table)
+    console.print(response_panel)
 
 
 if __name__ == "__main__":
