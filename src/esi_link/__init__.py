60a6d0749697cdc98f42de924d7b1fd5925dcbec
Chad Lowe <pfmsoft.dev@gmail.com>
2026-05-07 04:50:29 -0700

    version bump 0.3.0

--- src/esi_link/__init__.py
+++ src/esi_link/__init__.py
@@ -17,7 +17,7 @@ __url__ = "https://github.com/DonalChilde/esi-link"
 #######################################################################################
 __description__ = "A command line first interface to the Eve Online API"
 # The short X.Y.Z version.
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 # The full version, including alpha/beta/rc tags.
 __release__ = __version__
 #######################################################################################
